3f7bcb5be73ee411242923e57ddf089f970926e3
wb7898 <wb7898@arcsoft.com.cn>
2023-01-29 13:58:23 +0800

    1

--- c++/Android.mk
+++ c++/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := v4l2_test
+LOCAL_MODULE := x9u_v4l2_stream_capture
 LOCAL_SRC_FILES := main.cpp
 
 include $(BUILD_EXECUTABLE)
